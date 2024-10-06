eca1b0727fdd5060ba6c7ca5caccbd07c9d57767
Leonie <leonie@wargitsch.de>
2024-10-07 00:11:05 +0200

    forgot to push bux fix

--- Lil Mouse/Assets/Scripts/GamePlay/MouseMovement.cs
+++ Lil Mouse/Assets/Scripts/GamePlay/MouseMovement.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 
 public class MouseMovement : MonoBehaviour
